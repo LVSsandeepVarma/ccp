5f7f9899ce211c51a09382f432a647f6291fb5b9
Sandeep Varma LVS <sandeepvarmalvs@Sandeeps-Laptop.local>
2024-01-14 10:33:00 +0530

    added profile edit address form

--- staff/src/Pages/customerForms/AddressForm.jsx
+++ staff/src/Pages/customerForms/AddressForm.jsx
@@ -154,119 +154,108 @@ export default function AddressForm({ userInfo }) {
               Street
             </label>
             <textarea
-              name="name"
+              name="b_address"
               rows="2"
               className="form-control"
-              placeholder="Address"
+              placeholder="Billing Address"
+              {...register("b_address", {
+                required: true,
+                message: "Billing Address is required",
+              })}
             ></textarea>
-          </div>
-          <div className="mt-3">
-            <label htmlFor="placeholderInput" className="form-label">
-              City
-            </label>
-            <Select
-              className="js-example-basic-single"
-              name="city"
-              placeholder="Select City"
-              options={[
-                {
-                  value: "Alabama",
-                  label: "Alabama",
-                },
-                {
-                  value: "Madrid",
-                  label: "Madrid",
-                },
-                {
-                  value: "Toronto",
-                  label: "Toronto",
-                },
-                {
-                  value: "Londan",
-                  label: "Londan",
-                },
-                {
-                  value: "Wyoming",
-                  label: "Wyoming",
-                },
-              ]}
-            />
-          </div>
-          <div className="mt-3">
-            <label htmlFor="placeholderInput" className="form-label">
-              State
-            </label>
-            <Select
-              className="js-example-basic-single"
-              name="state"
-              placeholder="Select State"
-              options={[
-                {
-                  value: "Alabama",
-                  label: "Alabama",
-                },
-                {
-                  value: "Madrid",
-                  label: "Madrid",
-                },
-                {
-                  value: "Toronto",
-                  label: "Toronto",
-                },
-                {
-                  value: "Londan",
-                  label: "Londan",
-                },
-                {
-                  value: "Wyoming",
-                  label: "Wyoming",
-                },
-              ]}
-            />
-          </div>
-          <div className="mt-3">
-            <label htmlFor="placeholderInput" className="form-label">
-              Country
-            </label>
-            <Select
-              className="js-example-basic-single"
-              name="country"
-              placeholder="Select Country"
-              options={[
-                {
-                  value: "Alabama",
-                  label: "Alabama",
-                },
-                {
-                  value: "Madrid",
-                  label: "Madrid",
-                },
-                {
-                  value: "Toronto",
-                  label: "Toronto",
-                },
-                {
-                  value: "Londan",
-                  label: "Londan",
-                },
-                {
-                  value: "Wyoming",
-                  label: "Wyoming",
-                },
-              ]}
-            />
+            {
+              <span className="error text-red-600">
+                {errors?.b_address?.type == "required" && "Billing address is required"}
+                {errors?.b_address?.type == "apierr" && errors?.b_address?.message}
+              </span>
+            }
           </div>
           <div className="mt-3">
             <label htmlFor="placeholderInput" className="form-label">
               Zip Code
             </label>
             <input
-              type="text"
-              className="form-control"
-              id=""
-              placeholder="Zip Code"
-            />
+                type="number"
+                className="form-control"
+                id="pinInput"
+                placeholder="Enter Postal Code"
+                {...register("b_zip", {
+                  required: true,
+                  message: "Billing Zip code is required",
+                })}
+              />
+              {
+                <span className="error text-red-600">
+                  {errors?.b_zip?.type == "required" && "Zip code is required"}
+                  {errors?.b_zip?.type == "apierr" && errors?.b_zip?.message}
+                </span>
+              }
           </div>
+          <div className="mt-3">
+            <label htmlFor="placeholderInput" className="form-label">
+              City
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="cityInput"
+                placeholder="Enter City"
+                {...register("b_city", {
+                  required: true,
+                  message: "Billing City is required",
+                })}
+              />
+              {
+                <span className="error text-red-600">
+                  {errors?.b_city?.type == "required" && "City is required"}
+                  {errors?.b_city?.type == "apierr" && errors?.b_city?.message}
+                </span>
+              }
+          </div>
+          <div className="mt-3">
+            <label htmlFor="placeholderInput" className="form-label">
+              State
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="stateInput"
+                placeholder="Enter State"
+                {...register("b_state", {
+                  required: true,
+                  message: "Billing state is required",
+                })}
+              />
+              {
+                <span className="error text-red-600">
+                  {errors?.b_state?.type == "required" && "State is required"}
+                  {errors?.b_state?.type == "apierr" && errors?.b_state?.message}
+                </span>
+              }
+          </div>
+          <div className="mt-3">
+            <label htmlFor="placeholderInput" className="form-label">
+              Country
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="countryInput"
+                placeholder="Enter Country"
+                {...register("b_country", {
+                  required: true,
+                  message: "Billing Country is required",
+                })}
+              />
+              {
+                <span className="error text-red-600">
+                  {errors?.b_country?.type == "required" && "Country is required"}
+                  {errors?.b_country?.type == "apierr" &&
+                    errors?.b_country?.message}
+                </span>
+              }
+          </div>
+          
         </div>
         <div className="col-lg-6">
           <div className="d-flex align-items-center justify-content-between border-bottom pb-2">
@@ -280,120 +269,146 @@ export default function AddressForm({ userInfo }) {
               Street
             </label>
             <textarea
-              name="name"
+              name="s_address"
               rows="2"
               className="form-control"
-              placeholder="Address"
+              placeholder="Shipping Address"
+              {...register("s_address", {
+                required: true,
+                message: "Shipping Address is required",
+              })}
             ></textarea>
-          </div>
-          <div className="mt-3">
-            <label htmlFor="placeholderInput" className="form-label">
-              City
-            </label>
-            <Select
-              className="js-example-basic-single"
-              name="city"
-              placeholder="Select City"
-              options={[
-                {
-                  value: "Alabama",
-                  label: "Alabama",
-                },
-                {
-                  value: "Madrid",
-                  label: "Madrid",
-                },
-                {
-                  value: "Toronto",
-                  label: "Toronto",
-                },
-                {
-                  value: "Londan",
-                  label: "Londan",
-                },
-                {
-                  value: "Wyoming",
-                  label: "Wyoming",
-                },
-              ]}
-            />
-          </div>
-          <div className="mt-3">
-            <label htmlFor="placeholderInput" className="form-label">
-              State
-            </label>
-            <Select
-              className="js-example-basic-single"
-              name="state"
-              placeholder="Select State"
-              options={[
-                {
-                  value: "Alabama",
-                  label: "Alabama",
-                },
-                {
-                  value: "Madrid",
-                  label: "Madrid",
-                },
-                {
-                  value: "Toronto",
-                  label: "Toronto",
-                },
-                {
-                  value: "Londan",
-                  label: "Londan",
-                },
-                {
-                  value: "Wyoming",
-                  label: "Wyoming",
-                },
-              ]}
-            />
-          </div>
-          <div className="mt-3">
-            <label htmlFor="placeholderInput" className="form-label">
-              Country
-            </label>
-            <Select
-              className="js-example-basic-single"
-              name="country"
-              placeholder="Select Country"
-              options={[
-                {
-                  value: "Alabama",
-                  label: "Alabama",
-                },
-                {
-                  value: "Madrid",
-                  label: "Madrid",
-                },
-                {
-                  value: "Toronto",
-                  label: "Toronto",
-                },
-                {
-                  value: "Londan",
-                  label: "Londan",
-                },
-                {
-                  value: "Wyoming",
-                  label: "Wyoming",
-                },
-              ]}
-            />
+            {
+              <span className="error text-red-600">
+                {errors?.s_address?.type == "required" && "Shipping address is required"}
+                {errors?.s_address?.type == "apierr" && errors?.s_address?.message}
+              </span>
+            }
           </div>
           <div className="mt-3">
             <label htmlFor="placeholderInput" className="form-label">
               Zip Code
             </label>
             <input
-              type="text"
-              className="form-control"
-              id=""
-              placeholder="Zip Code"
-            />
+                type="number"
+                className="form-control"
+                id="pinInput"
+                placeholder="Enter Postal Code"
+                {...register("s_zip", {
+                  required: true,
+                  message: "Shipping Zip code is required",
+                })}
+              />
+              {
+                <span className="error text-red-600">
+                  {errors?.s_zip?.type == "required" && "Zip code is required"}
+                  {errors?.s_zip?.type == "apierr" && errors?.s_zip?.message}
+                </span>
+              }
+          </div>
+          <div className="mt-3">
+            <label htmlFor="placeholderInput" className="form-label">
+              City
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="cityInput"
+                placeholder="Enter City"
+                {...register("s_city", {
+                  required: true,
+                  message: "Shipping City is required",
+                })}
+              />
+              {
+                <span className="error text-red-600">
+                  {errors?.s_city?.type == "required" && "City is required"}
+                  {errors?.s_city?.type == "apierr" && errors?.s_city?.message}
+                </span>
+              }
+          </div>
+          <div className="mt-3">
+            <label htmlFor="placeholderInput" className="form-label">
+              State
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="stateInput"
+                placeholder="Enter State"
+                {...register("s_state", {
+                  required: true,
+                  message: "Shipping State tate is required",
+                })}
+              />
+              {
+                <span className="error text-red-600">
+                  {errors?.s_state?.type == "required" && "State is required"}
+                  {errors?.s_state?.type == "apierr" && errors?.s_state?.message}
+                </span>
+              }
+          </div>
+          <div className="mt-3">
+            <label htmlFor="placeholderInput" className="form-label">
+              Country
+            </label>
+            <input
+                type="text"
+                className="form-control"
+                id="countryInput"
+                placeholder="Enter Country"
+                {...register("s_country", {
+                  required: true,
+                  message: "Shipping Country is required",
+                })}
+              />
+              {
+                <span className="error text-red-600">
+                  {errors?.s_country?.type == "required" && "Country is required"}
+                  {errors?.s_country?.type == "apierr" &&
+                    errors?.s_country?.message}
+                </span>
+              }
           </div>
         </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="">
+            <p className="text-danger text-center">{apiErr}</p>
+            <p className="text-success text-center">{successMsg}</p>
+          </div>
+
+          <div className="hstack gap-2 justify-content-end mt-3">
+            <div className="text-end">
+              {!isLoading && (
+                <button
+                  type="submit"
+                  className="btn btn-primary bg-primary"
+                  id="sa-success"
+                  // onClick={() => handleSubmit(onSubmit())}
+                >
+                  Save
+                </button>
+              )}
+              {isLoading && (
+                <button
+                  disabled
+                  type="button"
+                  className="btn btn-outline-primary btn-load"
+                >
+                  <span className="d-flex align-items-center">
+                    <span className="flex-grow-1 me-2">Please Wait...</span>
+                    <span
+                      className="spinner-border flex-shrink-0"
+                      role="status"
+                    >
+                      <span className="visually-hidden">Please Wait...</span>
+                    </span>
+                  </span>
+                </button>
+              )}
+            </div>
+          </div>
         </div>
         </form>
     </>
